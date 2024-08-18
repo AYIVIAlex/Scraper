ce369b9d4391b9ad029c86b238f9be00dba2564d
AYIVI Alex~ <ayivialex19@gmail.com>
2024-08-18 22:46:09 +0100

    modification de la méthode de choix des dossiers de téléchargement

--- selectfileandfolder.py
+++ selectfileandfolder.py
@@ -1,18 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
 
-def select_file():
-    # Crée une instance de la fenêtre tkinter
-    root = tk.Tk()
-    # Masque la fenêtre principale
-    root.withdraw()
-    # Ouvre la boîte de dialogue pour sélectionner un fichier
-    file_selected = filedialog.askopenfilename(
-        title="Select a file",  # Titre de la boîte de dialogue
-        filetypes=(("All files", "*.*"), ("Text files", "*.txt"), ("CSV files", "*.csv"))  # Types de fichiers à afficher
-    )
-    return file_selected
-
 def select_directory():
     # Crée une instance de la fenêtre tkinter
     root = tk.Tk()
